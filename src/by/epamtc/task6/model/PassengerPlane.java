d86b1f9ef6ca54ac1cb150c3126df774ab5bfd95
Annette <oiveshka@gmail.com>
2021-05-21 10:08:40 +0300

    add more fields

--- src/by/epamtc/task6/model/PassengerPlane.java
+++ src/by/epamtc/task6/model/PassengerPlane.java
@@ -2,9 +2,29 @@ package by.epamtc.task6.model;
 
 import java.io.Serializable;
 
-public class PassengerPlane extends Plane implements Serializable,Cloneable {
+public class PassengerPlane extends Plane implements Serializable, Cloneable {
     private int engineCount;
     private int vipPassengerSeatsCount;
+    private int passengerSeatsCount;
+    private int flightAttendantsCount;
+    private int toiletsCount;
+
+
+    public int getToiletsCount() {
+        return toiletsCount;
+    }
+
+    public void setToiletsCount(int toiletsCount) {
+        this.toiletsCount = toiletsCount;
+    }
+
+    public int getFlightAttendantsCount() {
+        return flightAttendantsCount;
+    }
+
+    public void setFlightAttendantsCount(int flightAttendantsCount) {
+        this.flightAttendantsCount = flightAttendantsCount;
+    }
 
     public int getVipPassengerSeatsCount() {
         return vipPassengerSeatsCount;
@@ -14,32 +34,33 @@ public class PassengerPlane extends Plane implements Serializable,Cloneable {
         this.vipPassengerSeatsCount = vipPassengerSeatsCount;
     }
 
-
-    @Override
-    double getFuelConsumption() {
-        return getPassengerCapacity() * getEngineCount();
+    public int getPassengerSeatsCount() {
+        return passengerSeatsCount;
     }
 
-   /* public PassengerPlane(String title, double carryCapacity, int peopleCapacity, int engineCount) {
-        super(title, carryCapacity, peopleCapacity);
-        this.engineCount = engineCount;
+    public void setPassengerSeatsCount(int passengerSeatsCount) {
+        this.passengerSeatsCount = passengerSeatsCount;
     }
-
-    public PassengerPlane(PassengerPlane other) {
-        super(other);
-        this.engineCount = other.engineCount;
-    }*/
 
     public PassengerPlane(PassengerPlane other) {
         super(other);
         this.engineCount = other.engineCount;
         this.vipPassengerSeatsCount = other.vipPassengerSeatsCount;
+        this.passengerSeatsCount = other.passengerSeatsCount;
+        this.flightAttendantsCount = other.flightAttendantsCount;
+        this.toiletsCount = other.toiletsCount;
     }
 
-    public PassengerPlane(String title, double cargoCapacity, int passengerCapacity, int engineCount, int vipPassengerSeatsCount) {
+
+    public PassengerPlane(String title, double cargoCapacity, int passengerCapacity, int engineCount,
+                          int vipPassengerSeatsCount, int passengerSeatsCount, int flightAttendantsCount,
+                          int toiletsCount) {
         super(title, cargoCapacity, passengerCapacity);
         this.engineCount = engineCount;
         this.vipPassengerSeatsCount = vipPassengerSeatsCount;
+        this.passengerSeatsCount = passengerSeatsCount;
+        this.flightAttendantsCount = flightAttendantsCount;
+        this.toiletsCount = toiletsCount;
     }
 
     public int getEngineCount() {
@@ -50,7 +71,8 @@ public class PassengerPlane extends Plane implements Serializable,Cloneable {
         this.engineCount = engineCount;
     }
 
-    @Override
+
+   /* @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -62,16 +84,21 @@ public class PassengerPlane extends Plane implements Serializable,Cloneable {
     public int hashCode() {
         int hash = super.hashCode();
         return hash = (int) (31 * (engineCount + getFuelConsumption()));
-    }
+    }*/
 
-    @Override
+   /* @Override
     public String toString() {
         return super.toString() + " engine count= " + engineCount + " fuel consumption= " + getFuelConsumption();
-    }
+    }*/
 
     @Override
     public PassengerPlane clone() throws CloneNotSupportedException {
         return (PassengerPlane) super.clone();
+    }
+
+    @Override
+    double getFuelConsumption() {
+        return getPassengerCapacity() * getEngineCount();
     }
 
 }
